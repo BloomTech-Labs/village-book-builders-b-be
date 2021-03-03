a25fa8c5cb94055b83761a7bd3e09bf950f5cf14
Mark Ambrocio <wowmarkambro@gmail.com>
2021-03-02 19:59:52 -0600

    fix cors issues (#2)

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const port = process.env.PORT || 5000;
-const jsonServer = require('json-server')
-const auth = require('json-server-auth')
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
 
 const app = jsonServer.create();
-const router = jsonServer.router('db.json')
+const router = jsonServer.router("db.json");
 
+app.use(cors());
+app.use(auth);
+app.use(router);
 
-app.use(auth)
-app.use(router)
-
-app.db = router.db
+app.db = router.db;
 //binding app to db
 
 app.use(router);
 
-app.listen(port, () => console.log(`\n** Running on port ${port} **\t http://localhost:${port}/\n`));
+app.listen(port, () =>
+  console.log(`\n** Running on port ${port} **\t http://localhost:${port}/\n`)
+);
