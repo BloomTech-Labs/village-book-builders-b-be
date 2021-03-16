d4ce2f0abbe9a3654a79fbda4b9226ec02f98c33
amado4884 <amado4884@gmail.com>
2021-03-15 23:47:06 -0400

    added proper authentication

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const port = process.env.PORT || 5000;
 const jsonServer = require('json-server-relationship');
 const auth = require('json-server-auth');
+const fs = require('fs');
 const cors = require('cors');
 
 const app = jsonServer.create();
 const router = jsonServer.router('db.json');
 
+const routes = JSON.parse(fs.readFileSync('./routes.json'));
+const rules = auth.rewriter(routes);
+
 app.use(cors());
+app.use(rules);
 app.use(auth);
 app.use(router);
 
